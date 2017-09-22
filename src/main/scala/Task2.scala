22cad07ec7f6c7d97df84e53543d0f926924f5e7
Pavel Beschetnov <geomslayer@gmail.com>
2017-09-22 23:45:39 +0300

    Fix types in task 2

--- src/main/scala/Task2.scala
+++ src/main/scala/Task2.scala
@@ -1,10 +1,10 @@
 object Task2 extends App {
 
-  def collatzSteps(x: BigInt, steps: Long = 0) =
+  def collatzSteps(x: BigInt, steps: Long = 0): Long =
     if (x == 1) steps
     else collatzSteps(if (x % 2 == 0) x / 2 else 3 * x + 1, steps + 1)
 
-  def collatzSeq(x, seq = Vector.empty): Seq[BigInt] =
+  def collatzSeq(x: BigInt, seq: Seq[BigInt] = Vector.empty): Seq[BigInt] =
     if (x == 1) seq
     else collatzSeq(if (x % 2 == 0) x / 2 else 3 * x + 1, seq :+ x)
 
