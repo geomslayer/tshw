426d2a8e7bfab182fcd23c3276ed239a007baecc
Pavel Beschetnov <geomslayer@gmail.com>
2017-09-22 23:36:44 +0300

    Start task 2

---
+++ src/main/scala/Task2.scala
@@ -0,0 +1,11 @@
+object Task2 extends App {
+
+  def collatzSteps(x: BigInt, steps: Long = 0) =
+    if (x == 1) steps
+    else collatzSteps(if (x % 2 == 0) x / 2 else 3 * x + 1, steps + 1)
+
+  def collatzSeq(x, seq = Vector.empty): Seq[BigInt] =
+    if (x == 1) seq
+    else collatzSeq(if (x % 2 == 0) x / 2 else 3 * x + 1, seq :+ x)
+
+}
